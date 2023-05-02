e32a54caa43464e1c0847b08c8dd2a9bdafaaee0
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-02 10:56:33 +0200

    feat: day2_part2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use std::fs;
 
 fn main() {
-    let result = day2();
+    let result = day2_part2();
     println!("result: {}", result);
 }
 
@@ -60,22 +60,53 @@ enum Move {
 }
 
 impl Move {
+    fn value(&self) -> u32 {
+        match &self {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3,
+        }
+    }
+
+    fn reverse_pair(&self, letter: &str) -> Self {
+        match &self {
+            Move::Rock => match letter {
+                "X" => Move::Scissors,
+                "Y" => Move::Rock,
+                "Z" => Move::Paper,
+                _ => panic!("unknown option"),
+            },
+            Move::Paper => match letter {
+                "X" => Move::Rock,
+                "Y" => Move::Paper,
+                "Z" => Move::Scissors,
+                _ => panic!("unknown option"),
+            },
+            Move::Scissors => match letter {
+                "X" => Move::Paper,
+                "Y" => Move::Scissors,
+                "Z" => Move::Rock,
+                _ => panic!("unknown option"),
+            },
+        }
+    }
+
     fn compare(&self, other: &Move) -> u32 {
         match &self {
             Move::Rock => match other {
-                Move::Rock => 1 + 3,
-                Move::Paper => 1 + 0,
-                Move::Scissors => 1 + 6,
+                Move::Rock => self.value() + 3,
+                Move::Paper => self.value() + 0,
+                Move::Scissors => self.value() + 6,
             },
             Move::Paper => match other {
-                Move::Rock => 2 + 6,
-                Move::Paper => 2 + 3,
-                Move::Scissors => 2 + 0,
+                Move::Rock => self.value() + 6,
+                Move::Paper => self.value() + 3,
+                Move::Scissors => self.value() + 0,
             },
             Move::Scissors => match other {
-                Move::Rock => 3 + 0,
-                Move::Paper => 3 + 6,
-                Move::Scissors => 3 + 3,
+                Move::Rock => self.value() + 0,
+                Move::Paper => self.value() + 6,
+                Move::Scissors => self.value() + 3,
             },
         }
     }
@@ -98,6 +129,23 @@ fn day2() -> u32 {
             "Z" => Move::Scissors,
             _ => panic!("unknown option"),
         };
+        sum += ours.compare(&theirs);
+    }
+    sum
+}
+
+fn day2_part2() -> u32 {
+    let contents = fs::read_to_string("assets/real/day2").expect("Missing file");
+    let mut sum = 0;
+    for line in contents.lines() {
+        let parts: Vec<&str> = line.split(' ').collect();
+        let theirs = match parts[0] {
+            "A" => Move::Rock,
+            "B" => Move::Paper,
+            "C" => Move::Scissors,
+            _ => panic!("unknown option"),
+        };
+        let ours = theirs.reverse_pair(parts[1]);
         sum += ours.compare(&theirs);
     }
     sum
